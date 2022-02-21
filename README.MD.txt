e7a1f3089067ca1a145ad42456bcc3d8bd67606b
HOARAU Thomas <perso.thomas.hoarau@gmail.com>
2022-02-21 15:06:46 +0100

    add new function to extract barcode from bitmap

---
+++ README.MD.txt
@@ -0,0 +1,25 @@
+# Projet IA Android
+
+![start image](https://github.com/hoaraut35/P9/blob/main/blob/mockup.JPG)
+
+
+This is a demo project to extract a barcode
+
+## Design
+Material Design
+
+## Platform
+Phone
+
+## Functions
+Extract barcode from Bitmap
+
+## Development
+- Kotlin 
+- Activity / Fragments
+
+## Api 
+- Google Machine Learning
+
+## Developper
+HOARAU THomas
--- app/src/main/java/com/testmyteam/myia/MainActivity.kt
+++ app/src/main/java/com/testmyteam/myia/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.testmyteam.myia
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.common.InputImage
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //setup the barcode scanner...
+        val options = BarcodeScannerOptions.Builder()
+            .setBarcodeFormats(
+                Barcode.FORMAT_QR_CODE,
+                Barcode.FORMAT_AZTEC,
+                Barcode.FORMAT_EAN_13
+            )
+            .build()
+
+        //setup image ...
+        val image = BitmapFactory.decodeResource(resources, R.drawable.codebarre)
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        imageView.setImageResource(R.drawable.codebarre)
+
+
+        //setup inputImage ...
+        val image2 = InputImage.fromBitmap(image, 0)
+
+        //setup instance of scanner
+        val scanner = BarcodeScanning.getClient()
+
+        //setup the button ...
+        val button = findViewById<Button>(R.id.btn)
+        val textView = findViewById<TextView>(R.id.textView)
+
+        button.setOnClickListener {
+
+            val result = scanner.process(image2).addOnSuccessListener { barcodelist ->
+                for (barcode in barcodelist) {
+                    val bounds = barcode.boundingBox
+                    val corners = barcode.cornerPoints
+                    val rawValue = barcode.rawValue
+                    val valueType = barcode.valueType
+                    Toast.makeText(this, "Extract barcode " + rawValue.toString(), Toast.LENGTH_SHORT).show()
+                    textView.text = "Data : " + rawValue.toString()
+
+                }
+
+            }.addOnFailureListener {
+                Toast.makeText(this, "Can't extract barcode " + it.message, Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+        }
     }
+
 }
