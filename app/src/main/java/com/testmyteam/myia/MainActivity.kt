1a809fca7e60696ab67e410f3cc8762d4af5ca38
HOARAU Thomas <perso.thomas.hoarau@gmail.com>
2022-02-22 11:08:06 +0100

    add new function to detect face

--- app/src/main/java/com/testmyteam/myia/MainActivity.kt
+++ app/src/main/java/com/testmyteam/myia/MainActivity.kt
@@ -11,6 +11,10 @@ import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.FaceContour
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetectorOptions
+import com.google.mlkit.vision.face.FaceLandmark
 
 class MainActivity : AppCompatActivity() {
 
@@ -51,7 +55,11 @@ class MainActivity : AppCompatActivity() {
                     val corners = barcode.cornerPoints
                     val rawValue = barcode.rawValue
                     val valueType = barcode.valueType
-                    Toast.makeText(this, "Extract barcode " + rawValue.toString(), Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        this,
+                        "Extract barcode " + rawValue.toString(),
+                        Toast.LENGTH_SHORT
+                    ).show()
                     textView.text = "Data : " + rawValue.toString()
 
                 }
@@ -61,7 +69,83 @@ class MainActivity : AppCompatActivity() {
                     .show()
             }
 
+
         }
+
+
+        //setup face detection...
+        val highAccuracyOpts = FaceDetectorOptions.Builder()
+             //select better mode
+            .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
+             //identitify eyes ears, noze , etc
+            .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
+             //get data
+            .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
+            .build()
+
+        // Real-time contour detection
+        val realTimeOpts = FaceDetectorOptions.Builder()
+            .setContourMode(FaceDetectorOptions.CONTOUR_MODE_ALL)
+            .build()
+
+        //setup image ...
+        val photo = BitmapFactory.decodeResource(resources, R.drawable.photo)
+        val imageViewPhoto = findViewById<ImageView>(R.id.imageViewPhoto)
+        imageViewPhoto.setImageResource(R.drawable.photo)
+
+        //setup inputImage ...
+        val photo2 = InputImage.fromBitmap(photo, 0)
+
+        val detector = FaceDetection.getClient(highAccuracyOpts)
+
+
+        //setup the button ...
+        val buttonPhoto = findViewById<Button>(R.id.btnphoto)
+
+        buttonPhoto.setOnClickListener {
+            val result = detector.process(photo2)
+                .addOnSuccessListener { faces ->
+                    Toast.makeText(this, " Visage trouvé ", Toast.LENGTH_SHORT)
+                        .show()
+
+
+                    for (face in faces) {
+                        val bounds = face.boundingBox
+                        val rotY = face.headEulerAngleY // Head is rotated to the right rotY degrees
+                        val rotZ = face.headEulerAngleZ // Head is tilted sideways rotZ degrees
+
+                        // If landmark detection was enabled (mouth, ears, eyes, cheeks, and
+                        // nose available):
+                        val leftEar = face.getLandmark(FaceLandmark.LEFT_EAR)
+                        leftEar?.let {
+                            val leftEarPos = leftEar.position
+                        }
+
+                        // If contour detection was enabled:
+                        val leftEyeContour = face.getContour(FaceContour.LEFT_EYE)?.points
+                        val upperLipBottomContour = face.getContour(FaceContour.UPPER_LIP_BOTTOM)?.points
+
+                        // If classification was enabled:
+                        if (face.smilingProbability != null) {
+                            val smileProb = face.smilingProbability
+                            Toast.makeText(this,"smile",Toast.LENGTH_SHORT).show()
+                        }
+                        if (face.rightEyeOpenProbability != null) {
+                            val rightEyeOpenProb = face.rightEyeOpenProbability
+                        }
+
+                        // If face tracking was enabled:
+                        if (face.trackingId != null) {
+                            val id = face.trackingId
+                        }
+                    }
+                }
+                .addOnFailureListener { e ->
+                    Toast.makeText(this, "Can't extract photo " + e.message, Toast.LENGTH_SHORT)
+                        .show()
+                }
+        }
+
     }
 
 }
